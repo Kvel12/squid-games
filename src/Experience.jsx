69c02870cf3086646caa5bf6fbad90258c3090b2
Kevin Velez <kevin.alejandro.velez@correounivalle.edu.co>
2024-04-24 10:21:37 -0500

    upload

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,38 +1,12 @@
-import { OrbitControls } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
-import { useRef } from "react";
+import { AvatarProvider } from "./context/AvatarContext";
+import RoutesSquidGames from "./routes/RoutesSquidGames"
 
 const Experience = () => {
-    const boxRef = useRef(null);
-    
-    useFrame((state, delta)=>{
-        boxRef.current.rotation.x += 1 * delta;
-    })
-
     return (
-        <>
-            <ambientLight />
-            <directionalLight position={[10, 10, 5]} />
-            <OrbitControls makeDefault />
-            <mesh ref={boxRef}>
-                <boxGeometry args={[1, 1, 1]} />
-                <meshStandardMaterial color={"purple"} />
-            </mesh>
-        </>
-
+        <AvatarProvider>
+            <RoutesSquidGames />
+        </AvatarProvider>
     )
 }
 
 export default Experience;
-
-// const Experience = ({ title, subtitle }) => {
-
-//     return (
-//         <>
-//             <h1 className="title"> {title} </h1>
-//             <span> {subtitle} </span>
-//         </>
-//     )
-// }
-
-// export default Experience;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
+import Experience from "./Experience";
 import "./styles.css";
 import { createRoot } from "react-dom/client";
-import Experience from "./Experience";
-import { Canvas } from "@react-three/fiber";
 
 const root = createRoot(document.getElementById("root"));
 
-root.render(
-    <Canvas
-        camera={
-            {
-                position: [2, 3, 5]
-            }
-        }
-    >
-        <Experience />
-    </Canvas>
-
-    // <Experience 
-    //     title = "Squid Games"
-    //     subtitle = "3D Web"
-    // />
-)
+root.render(<Experience />);
